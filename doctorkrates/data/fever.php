a2ff13b983f3d6e2210196cf1e72448360bb3583
xilitolabs868 <xilitolabs868@gmail.com>
2021-03-15 21:14:51 +0900

    with dropdown menu

---
+++ doctorkrates/data/fever.php
@@ -0,0 +1,1 @@
+Fever, also referred to as pyrexia, is defined as having a temperature above the normal range due to an increase in the body's temperature set point. There is not a single agreed-upon upper limit for normal temperature with sources using values between 37.2 and 38.3 °C (99.0 and 100.9 °F) in humans.
--- doctorkrates/index.php
+++ doctorkrates/index.php
@@ -1,27 +1,55 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '111111',
+  'doctorkrates');
+
+$sql = "SELECT * FROM symptom";
+$result = mysqli_query($conn, $sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <link rel="stylesheet" href="./css/dropdown.css">
+    <script src="dropdown.js"></script>
+
+    </script>
+    <title>Doctorkrates</title>
   </head>
   <body>
-    <?php
-    $conn = mysqli_connect(
-      'localhost',
-      'root',
-      '111111',
-      'doctorkrates');
-
-    $sql = "SELECT * FROM symptom";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_array($result);
-      $i = 0;
-      while($i < 5) {
-        echo $row[2];
-        $i = $i + 1;
-        $row = mysqli_fetch_array($result);
-        echo "<br>";
+    <ul>
+      <?php
+        while($row = mysqli_fetch_array($result)) {
+          $escaped_cc = htmlspecialchars($row['name']);
+          echo '<a href = ./index.php?id='.$escaped_cc.'>'.$escaped_cc.'</a><br>';
+        }
+      ?>
+      <a href="./ddx.php"><input type="button" name="next_page" value="Select!"></a>
+    </ul>
+    <h2>
+      <?php
+      if(isset($_GET['id'])) {
+        echo $_GET['id'];
+      } else {
+        echo 'Select your main symptom!';
       }
-    ?>
+      ?>
+    </h2>
+      <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="ccDropdown" class="dropdown-content">
+          <input type="text" placeholder="Seach your main symptom.." id="ccInput" onkeyup="filterFunction()">
+          Aasefasef
+          <a href="#base">Base</a>
+          <a href="#blog">Blog</a>
+          <a href="#contact">Contact</a>
+          <a href="#custom">Custom</a>
+          <a href="#support">Support</a>
+          <a href="#tools">Tools</a>
+      </div>
+    </div>
   </body>
 </html>
